22a866978ac1d19c546c8859636678ef2e1c9813
Dmitry Sidorov <sidorovdmitri6@mail.ru>
2022-07-11 20:57:07 +0300

    implemented create and drop methods

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,10 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -11,9 +14,39 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
 
+        String sql = "CREATE TABLE users (\n" +
+                "    id BIGINT PRIMARY KEY,\n" +
+                "    name VARCHAR(50),\n" +
+                "    last_name VARCHAR(50),\n" +
+                "    age SMALLINT);";
+
+        Statement statement = Util.getStatement();
+
+        if (statement != null){
+
+            try {
+                statement.execute(sql);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 
     public void dropUsersTable() {
+
+        String sql = "drop table users";
+
+        Statement statement = Util.getStatement();
+
+        if (statement != null){
+
+            try {
+                statement.execute(sql);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,36 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String url = "jdbc:postgresql://localhost:5432/testDB";
+    private static final String password = "Aluc1988";
+    private static final String user = "postgres";
+
+
+
+
+    public static Connection getConnection() {
+        try {
+            System.out.println("testconnection");
+            return  DriverManager.getConnection(url,
+                    user,
+                    password);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Statement getStatement() {
+        try {
+            return getConnection().createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
